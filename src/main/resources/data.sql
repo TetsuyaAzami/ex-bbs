fab1d39846b28d69835f20147546b7636c0bc9f8
Tetsuya Azami <zqianjian35@gmail.com>
2022-02-23 08:21:22 +0900

    いいね機能を実装しました

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,16 @@
-INSERT INTO articles(name, content) VALUES('伊賀', 'この掲示板について');
-INSERT INTO articles(name, content) VALUES('山田', '新たな投稿です。');
-INSERT INTO comments(name, content, article_id) VALUES('佐藤1', '伊賀さん書き込みのコメント1',1);
-INSERT INTO comments(name, content, article_id) VALUES('佐藤2', '伊賀さん書き込みのコメント2',1);
-INSERT INTO comments(name, content, article_id) VALUES('佐藤3', '伊賀さん書き込みのコメント3',1);
-INSERT INTO comments(name, content, article_id) VALUES('吉田1', '山田さん書き込みのコメント1',2);
-INSERT INTO comments(name, content, article_id) VALUES('吉田2', '山田さん書き込みのコメント2',2);
+--記事を1万5千件作成
+INSERT INTO articles(name, content)
+SELECT format('山田テストユーザー%s', i),format('山田記事テスト%s',i)
+FROM generate_series(1,5000) as i;
+INSERT INTO articles(name, content)
+SELECT format('鈴木テストユーザー%s', i),format('鈴木記事テスト%s',i)
+FROM generate_series(5001,10000) as i;
+INSERT INTO articles(name, content)
+SELECT format('佐藤テストユーザー%s', i),format('佐藤テスト%s',i)
+FROM generate_series(10001,15000) as i;
+
+
+--コメントを1万件作成(記事idは1~15000でランダム)
+INSERT INTO comments(name, content, article_id)
+SELECT format('テストユーザー%s', (random() * 15000)::int + 1),format('コメントテスト%s',i), (random() * 15000)::int + 1
+FROM generate_series(1,10000) AS i;
